b4a703e27095cfbbc1cd70cf1d2a36f235a37c7f
aluno <aluno@lab-wbs.local>
2016-03-30 08:10:50 -0300

    Exercicio de exibir numeros Pares e Impares ate 100

---
+++ vinicius.php
@@ -0,0 +1,23 @@
+﻿<html>
+<head>
+<title>vinicius</title>
+</head>
+<body>
+<?php
+$num2 = 2;
+	
+
+for($num1 =1; $num1 < 4000000; $num1++){
+	if ( ($num1 % $num2) == 0) {
+		echo "<br/>Numero Par: " . $num1;
+		
+	}if ( ($num1 % $num2) == 1) {
+		echo "<br/>Numero Impar: " . $num1;
+		}
+}
+?>
+
+
+
+</body>
+</html>
